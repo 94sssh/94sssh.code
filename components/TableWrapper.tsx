9f8df1cf0ce4f65acd426054f5d7abd7f4390e84
94sssh <abizsh@naver.com>
2023-12-23 23:52:34 +0900

    design: Table 태그 max-width 수정 및 가운데 정렬

--- components/TableWrapper.tsx
+++ components/TableWrapper.tsx
@@ -1,6 +1,6 @@
 const TableWrapper = ({ children }) => {
   return (
-    <div className="w-full overflow-x-auto">
+    <div className="mx-auto w-full max-w-2xl overflow-x-auto">
       <table>{children}</table>
     </div>
   )
