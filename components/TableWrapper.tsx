8c0b99c9493bc9ea22cbbcd8105c7f0fe22d9906
94sssh <abizsh@naver.com>
2024-06-21 00:30:30 +0900

    design: 표 Table 컴포넌트 width 속성 수정

--- components/TableWrapper.tsx
+++ components/TableWrapper.tsx
@@ -1,6 +1,6 @@
 const TableWrapper = ({ children }) => {
   return (
-    <div className="mx-auto w-full max-w-2xl overflow-x-auto">
+    <div className="mx-auto w-fit overflow-x-auto">
       <table>{children}</table>
     </div>
   )
